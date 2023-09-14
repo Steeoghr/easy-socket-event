ede26bb60b254017bb395004068c9dcb43c9a3f3
steo <stefano.gherardi.fl@gmail.com>
2023-09-14 15:49:18 +0200

    fix index export

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { SocketServer } from './server';
 import { SocketClient } from './client';
+import {createServer} from './server/functions';
+import {createClient} from './client/functions';
 
 export {
     SocketServer,
-    SocketClient
+    SocketClient,
+    createServer,
+    createClient
 }
