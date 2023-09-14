90e2f9faf79b8b3a8e02bea1580f1aa4fe951aa1
steo <stefano.gherardi.fl@gmail.com>
2023-09-14 22:57:04 +0200

    fix functions

--- client/functions.ts
+++ client/functions.ts
@@ -1,6 +1,6 @@
 import { SocketClient } from "../client";
 import io from "socket.io-client";
-import { IoClientSocket } from "types";
+import { IoClientSocket } from "../types";
 
 export function createClient(host: string, connected: () => void) {
     const socket: IoClientSocket = io(host);
