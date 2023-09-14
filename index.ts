9e8ed5f6d20dc0509ab5c68e395d9faace668ab2
steo <stefano.gherardi.fl@gmail.com>
2023-09-14 17:14:56 +0200

    fix functions export

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import { SocketServer } from './server';
 import { SocketClient } from './client';
-import {createServer} from './server/functions';
-import {createClient} from './client/functions';
+import {createServer as _createServer} from './server/functions';
+import {createClient as _createClient} from './client/functions';
+
+export function createServer() {
+    return _createServer();
+}
+export function createClient(host: string) {
+    return _createClient(host);
+}
 
 export {
     SocketServer,
     SocketClient,
-    createServer,
-    createClient
 }
