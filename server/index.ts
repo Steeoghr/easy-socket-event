4f82fce370af6387c9a1ff06d6e98875ae95971f
steo <stefano.gherardi.fl@gmail.com>
2023-09-14 23:08:59 +0200

    fix server

--- server/index.ts
+++ server/index.ts
@@ -35,6 +35,7 @@ export class SocketServer extends SocketActor implements IIoSocketServer {
     };
 
     private handleClose(socket: IoSocket) {
+        this.clientsConnected = this.clientsConnected.filter(client => client.conn.remoteAddress !== socket.conn.remoteAddress);
         console.log('Client disconnected:', socket.conn.remoteAddress);
         this.onClose(socket);
     };
