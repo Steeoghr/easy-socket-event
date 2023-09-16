cce5a6fa71b95ff588702bce24b8a29206f51252
steo <stefano.gherardi.fl@gmail.com>
2023-09-16 17:04:07 +0200

    TRY

--- server/functions.ts
+++ server/functions.ts
@@ -13,7 +13,7 @@ export function createServer() {
     // Configura EJS come motore di template
     app.set('view engine', 'ejs');
 // ../../../
-const views = '../../../../../views';
+const views = 'views';
     const relPath = path.join(__dirname, );
     console.log("relPath", relPath)
     // Specifica la cartella contenente i file HTML
@@ -22,18 +22,7 @@ const views = '../../../../../views';
     const server = http.createServer(app);
     const io = new Server(server);
     app.get("/", (req: any, res: any) => {
-        const _relPath = path.join(__dirname, views);
-        console.log("_relPath", _relPath)
-        const templatePath = path.join(_relPath, 'index.html');
-
-        // Verifica se il file del template esiste
-        if (fs.existsSync(templatePath)) {
-            // Passa dati al tuo template, se necessario
-            res.render('index');
-        } else {
-            // Se il file non esiste, restituisci un messaggio personalizzato
-            res.send('Server running... ' + templatePath);
-        }
+        res.render('index');
     })
     return new SocketServer(io, server, app);
 }
