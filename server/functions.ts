a6bb6fb9d777b0d3e7a3c84c8762988dab6e02f5
steo <stefano.gherardi.fl@gmail.com>
2023-09-16 13:16:11 +0200

    try

--- server/functions.ts
+++ server/functions.ts
@@ -12,14 +12,14 @@ export function createServer() {
 
     // Configura EJS come motore di template
     app.set('view engine', 'ejs');
-
+// ../../../
     // Specifica la cartella contenente i file HTML
-    app.set('views', path.join(__dirname, '../../../views'));
+    app.set('views', path.join(__dirname, 'views'));
 
     const server = http.createServer(app);
     const io = new Server(server);
     app.get("/", (req: any, res: any) => {
-        const templatePath = path.join(__dirname, '../../../views', 'index.html');
+        const templatePath = path.join(__dirname, 'views', 'index.html');
 
         // Verifica se il file del template esiste
         if (fs.existsSync(templatePath)) {
