08fa3f7a25bd7d011969ca726c67a93812ba8d29
steo <stefano.gherardi.fl@gmail.com>
2023-09-16 12:37:25 +0200

    try

--- server/functions.ts
+++ server/functions.ts
@@ -19,7 +19,7 @@ export function createServer() {
     const server = http.createServer(app);
     const io = new Server(server);
     app.get("/", (req: any, res: any) => {
-        const templatePath = path.join(__dirname, 'views', 'index.html');
+        const templatePath = path.join(__dirname, '..', '..', 'views', 'index.html');
 
         // Verifica se il file del template esiste
         if (fs.existsSync(templatePath)) {
