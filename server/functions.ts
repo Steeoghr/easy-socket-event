012f4337c208a084e4fa0e5eb0f7bed2fb40b813
stefano <stefanp.gherardi.fl@gmail.com>
2023-09-18 13:01:00 +0200

    cors to server

--- server/functions.ts
+++ server/functions.ts
@@ -8,7 +8,15 @@ import { SocketServer } from "../server";
 export function createServer() {
     const app = express();
     const server = http.createServer(app);
-    const io = new Server(server);
+    const io = new Server(server, {
+        cors: {
+            origin: [
+            'http://localhost:8081',
+            'https://chat-client-98edf.web.app',
+            // Altre origini consentite
+            ],
+        }
+    });
     app.get("/", (req: any, res: any) => {
         res.send("Server running...");
     })
