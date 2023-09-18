2cc727b8e82d774a4b9d3e38e51ea5ee8bc45a55
stefano <stefanp.gherardi.fl@gmail.com>
2023-09-18 13:53:05 +0200

    fix

--- server/functions.ts
+++ server/functions.ts
@@ -3,19 +3,9 @@ import {Server} from "socket.io";
 import http from "http";
 import { SocketServer } from "../server";
 
-export function createServer() {
-    const server = http.createServer(app);
-    const io = new Server(server, {
-        cors: {
-            origin: [
-            'http://localhost:8081',
-            'https://chat-client-98edf.web.app',
-            // Altre origini consentite
-            ],
-        }
-    });
-    app.get("/", (req: any, res: any) => {
-        res.send("Server running...");
-    })
-    return new SocketServer(io, server, app);
+export function createServer(expressApp: any) {
+
+    const server = http.createServer(expressApp);
+    const io = new Server(server);
+    return new SocketServer(io, server, expressApp);
 }
