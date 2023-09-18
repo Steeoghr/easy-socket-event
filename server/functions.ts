4d23d5680d49215864d0801ea9e01de5c0d97b77
stefano <stefanp.gherardi.fl@gmail.com>
2023-09-18 14:07:11 +0200

    cors to sserver

--- server/functions.ts
+++ server/functions.ts
@@ -3,9 +3,11 @@ import {Server} from "socket.io";
 import http from "http";
 import { SocketServer } from "../server";
 
-export function createServer(expressApp: any) {
+export function createServer(expressApp: any, cors?: any) {
 
     const server = http.createServer(expressApp);
-    const io = new Server(server);
+    const io = new Server(server, cors ? {
+        cors
+    } : undefined);
     return new SocketServer(io, server, expressApp);
 }
