5b04ea5d0e4834ff1e94ec27c45314cfc8ad5506
steo <stefano.gherardi.fl@gmail.com>
2023-10-02 09:44:00 +0200

    dd corsr origin option

--- server/functions.ts
+++ server/functions.ts
@@ -3,11 +3,14 @@ import {Server} from "socket.io";
 import http from "http";
 import { SocketServer } from "../server";
 
-export function createServer(expressApp: any, cors?: any) {
+export function createServer(expressApp: any, origin: string) {
 
     const server = http.createServer(expressApp);
-    const io = new Server(server, cors ? {
-        cors
-    } : undefined);
+    const io = new Server(server, {
+        cors:  {
+            origin,
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD", "CONNECT", "TRACE"],
+        }
+    });
     return new SocketServer(io, server, expressApp);
 }
